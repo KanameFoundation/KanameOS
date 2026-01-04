21b306fee2e3f1a407d163eeaae8ee0ecc4a44ba
DemuraAIdev <cyberaioff@gmail.com>
2026-01-05 03:52:50 +0700

    Change title

--- rspack.config.js
+++ rspack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/client/index.ejs'),
       favicon: path.resolve(__dirname, 'src/client/favicon.png'),
-      title: 'OS.js'
+      title: 'WebOS'
     }),
     new rspack.CopyRspackPlugin({
       patterns: [
